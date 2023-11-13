92b0644c4bf18aedc2c9134f794720a0779b8560
victoremanuelandrada <lvpemanuelandrada@gmail.com>
2023-11-12 22:46:02 -0300

    subo proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import reactLogo from './assets/react.svg'//importamos imagen y la llamamos en src
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {  //componente retorna etiquetas
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
